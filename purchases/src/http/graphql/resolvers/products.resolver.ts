4c113b1627010b4b9e6a1eccec0cf72ca9eb6229
Julio Felipe <julio@informach.com.br>
2022-05-14 19:23:31 -0300

    create models, resolvers and services

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,23 @@
+import { UseGuards } from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Product } from "../models/product";
+import { AuthorizationGuard } from "../../auth/authorization.guard";
+import { ProductsService } from "src/services/products.service";
+import { CreateProductInput } from "../inputs/create-product-input";
+
+@Resolver(() => Product)
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  //@UseGuards(AuthorizationGuard)
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
---
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -0,0 +1,52 @@
+import { UseGuards } from "@nestjs/common";
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { AuthUser, CurrentUser } from "src/http/auth/current-user";
+import { CustomersService } from "src/services/customers.service";
+import { ProductsService } from "src/services/products.service";
+import { PurchasesService } from "src/services/purchases.service";
+import { AuthorizationGuard } from "../../auth/authorization.guard";
+import { CreatePurchaseInput } from "../inputs/create-purchase-input";
+import { Product } from "../models/product";
+import { Purchase } from "../models/purchase";
+
+@Resolver(() => Purchase)
+export class PurchasesResolver {
+  constructor(
+    private purchasesService: PurchasesService,
+    private productService: ProductsService,
+    private customersService: CustomersService,
+  ) {}
+
+  @Query(() => [Purchase])
+  @UseGuards(AuthorizationGuard)
+  purchases() {
+    return this.purchasesService.listAllPurchases();
+  }
+
+  @ResolveField(() => Purchase)
+  product(@Parent() purchase: Purchase) {
+    return this.productService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput, 
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
+  }
+}
